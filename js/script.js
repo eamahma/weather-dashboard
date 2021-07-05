525a315990ca6e8ecc73428404c1d2b9e7f334c3
eamahma <eamahma@gmail.com>
2021-07-05 08:47:33 -0400

    even listener updated on two event; some cleanups

--- js/script.js
+++ js/script.js
@@ -3,6 +3,7 @@ var cityInputEl = document.querySelector('.input');
 var weatherContainerEl = document.querySelector('#weather-container');
 //var cityFormEl = document.querySelector('#city-form');
 var cityFormEl = document.querySelector('.button');
+var searchBoxEl = document.querySelector('#seachbox');
 var cityLat = 0;
 var cityLon = 0;
 const input = document.querySelector('input');
@@ -16,7 +17,7 @@ fetch(apiUrl)
       return response.json();
     })
     .then(function (data) {
-      console.log(data);
+//      console.log(data);
     });
 
 
@@ -24,13 +25,8 @@ fetch(apiUrl)
         event.preventDefault();
       
         var city = cityInputEl.value.trim();
-        console.log(city);
-  //      city_name = city;
-      
         if (city) {
            getCityWeather(city);
-      
-//          weatherContainerEl.textContent = '';
           cityInputEl.value = '';
         } else {
           alert('Please enter a City name');
@@ -41,7 +37,6 @@ fetch(apiUrl)
 var displayWeather = function (input) {
   var result = input.name;
   var currentDate = moment().format('L');
-//  result += " " + currentDate;
 $("#current-city").html("City: " + result);
 var weatherIcon = "http://openweathermap.org/img/wn/" + input.weather[0].icon + "@2x.png"
 $("#current-image").children("img").attr("src", weatherIcon);
@@ -53,13 +48,8 @@ $("#temp").html("Temp: " + input.main.temp + "&deg;F");
 }      
 
 var displayForcast = function (input) {
-//  var result = input.name;
-//  var currentDate = moment().format('L');
-// result += " " + currentDate;
-//  $(".current-city").html(result);
-//  $("#temp").html("Temp: " + input.main.temp + "&deg;F");
-//  $("#wind").html("Wind: " + input.wind.speed + " MPH");
-//  $("#humidity").html("Humidity: " + input.main.humidity + "%");
+
+  // UV index display on today forecast and background color
   $("#uvi-color").html(input.current.uvi);
   if (input.current.uvi < 3) {
     $("#uvi-color").attr("style", "background-color: lightgreen; padding: .5em; border-radius:20%")
@@ -71,53 +61,48 @@ var displayForcast = function (input) {
     $("#uvi-color").attr("style", "background-color: red; padding: .5em; border-radius:20%")
   }
 
+  // Day 1 forcast
   var date = moment().add(1,'days').format('L');
   $("#day1").html(date);
   var weatherIcon = "http://openweathermap.org/img/wn/" + input.daily[0].weather[0].icon + "@2x.png"
-  console.log(input.daily[0].weather[0].icon);
   $("#day1-image").children("img").attr("src", weatherIcon);
   $("#day1-temp").html("Temp: " + input.daily[0].temp.day + "&deg;F");
-  console.log(input.daily[0].temp.day);
   $("#day1-wind").html("Wind: " + input.daily[0].wind_speed + " MPH");
   $("#day1-humidity").html("Humidity: " + input.daily[0].humidity + "%");
 
+  // Day 2 forcast
   var date = moment().add(2,'days').format('L');
   $("#day2").html(date);
   var weatherIcon = "http://openweathermap.org/img/wn/" + input.daily[1].weather[0].icon + "@2x.png"
-  console.log(input.daily[1].weather[0].icon);
   $("#day2-image").children("img").attr("src", weatherIcon);
   $("#day2-temp").html("Temp: " + input.daily[1].temp.day + "&deg;F");
-  console.log(input.daily[1].temp.day);
   $("#day2-wind").html("Wind: " + input.daily[1].wind_speed + " MPH");
   $("#day2-humidity").html("Humidity: " + input.daily[1].humidity + "%");
 
-  var date = moment().add(2,'days').format('L');
+  // Day 3 forcast
+  var date = moment().add(3,'days').format('L');
   $("#day3").html(date);
   var weatherIcon = "http://openweathermap.org/img/wn/" + input.daily[2].weather[0].icon + "@2x.png"
-  console.log(input.daily[2].weather[0].icon);
   $("#day3-image").children("img").attr("src", weatherIcon);
   $("#day3-temp").html("Temp: " + input.daily[2].temp.day + "&deg;F");
-  console.log(input.daily[2].temp.day);
   $("#day3-wind").html("Wind: " + input.daily[2].wind_speed + " MPH");
   $("#day3-humidity").html("Humidity: " + input.daily[2].humidity + "%");
 
-  var date = moment().add(3,'days').format('L');
+  // Day 4 forcast
+  var date = moment().add(4,'days').format('L');
   $("#day4").html(date);
   var weatherIcon = "http://openweathermap.org/img/wn/" + input.daily[3].weather[0].icon + "@2x.png"
-  console.log(input.daily[3].weather[0].icon);
   $("#day4-image").children("img").attr("src", weatherIcon);
   $("#day4-temp").html("Temp: " + input.daily[3].temp.day + "&deg;F");
-  console.log(input.daily[3].temp.day);
   $("#day4-wind").html("Wind: " + input.daily[3].wind_speed + " MPH");
   $("#day4-humidity").html("Humidity: " + input.daily[3].humidity + "%");
 
-  var date = moment().add(4,'days').format('L');
+  // Day 5 forcast
+  var date = moment().add(5,'days').format('L');
   $("#day5").html(date);
   var weatherIcon = "http://openweathermap.org/img/wn/" + input.daily[4].weather[0].icon + "@2x.png"
-  console.log(input.daily[4].weather[0].icon);
   $("#day5-image").children("img").attr("src", weatherIcon);
   $("#day5-temp").html("Temp: " + input.daily[4].temp.day + "&deg;F");
-  console.log(input.daily[4].temp.day);
   $("#day5-wind").html("Wind: " + input.daily[4].wind_speed + " MPH");
   $("#day5-humidity").html("Humidity: " + input.daily[4].humidity + "%");
 
@@ -125,14 +110,12 @@ var displayForcast = function (input) {
 }  
 
 var getCityWeather = function (city) {
-//          console.log(city);
   var apiUrl = 'https://api.openweathermap.org/data/2.5/weather?q=' + city + '&units=imperial&appid=' + mykey;
 
   fetch(apiUrl)
     .then(function (response) {
       if (response.ok) {
         response.json().then(function (data) {
-          console.log(data);
           displayWeather(data);
           cityLat = data.coord.lat;
           cityLon = data.coord.lon;
@@ -155,7 +138,6 @@ var getCityForcast = function (latitude, longitude) {
       .then(function (response) {
         if (response.ok) {
           response.json().then(function (data) {
-            console.log(data);
             displayForcast(data);
           });
         } else {
@@ -167,12 +149,5 @@ var getCityForcast = function (latitude, longitude) {
       });
   };
 
-      cityFormEl.addEventListener('click', formSubmitHandler);
-      input.addEventListener('change', formSubmitHandler);
-
-//      console.log(city_name);
-
-//      var currentDate = moment().format('L');
-//      var currentDate = moment().date();
-      // console.log(currentDate);
-      // $(".current-city").html(city_name + currentDate);
+// eventlistner on both text change and dblclick
+  $('#searchbox').on('change dblclick', formSubmitHandler);
